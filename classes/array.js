e7a2ace17c70b11620b82a3b58d56f2942c17023
labor-day <shaffer.scott@yahoo.com>
2022-06-30 16:23:42 -0600

    reimplemented using ES5

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,14 @@
 // Your code here
+
+Array.prototype.isEqual = function(arr) {
+
+  return this.reduce(
+    (equal, element, index) => {
+      if (element !== arr[index]) {
+        equal = false;
+        return equal;
+      }
+      return equal;
+    }, true
+  );
+}
